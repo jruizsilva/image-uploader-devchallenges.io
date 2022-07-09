8f681af1dcc0a9fc4a49c7028f0899816255d424
jruizsilva <ruizsilvajonathan@gmail.com>
2022-07-09 17:51:55 -0300

    react app don't need dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
@@ -11,7 +9,7 @@ import store from "./redux/configureStore";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.REACT_APP_HEROKU || "http://localhost:3001";
+  process.env.REACT_APP_HEROKU_API || "http://localhost:3001";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
